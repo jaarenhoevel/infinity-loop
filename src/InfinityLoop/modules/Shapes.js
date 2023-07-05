3d141a24847f6a1ee763c220a5ab9b09fff00d7e
János Arenhövel <jarenhoevel@copting.de>
2023-07-05 15:14:07 +0200

    Add drawing methods

--- src/InfinityLoop/modules/Shapes.js
+++ src/InfinityLoop/modules/Shapes.js
@@ -14,8 +14,7 @@ class Shape {
         }
         
         this.rotation = rotation;
-        this.animationProgress = 0;
-        this.animationDirections = [0, 0, 0, 0];
+        this.animationProgress = [0, 0, 0, 0];
     }
 
     /**
@@ -45,10 +44,21 @@ class Shape {
 
     /**
      * Draw shape on canvas
-     * @param {context} context 
+     * @param {context} context
+     * @param {Object} settings 
      */
-    draw(context) {
+    draw(context, settings) {
         throw new Error("Must be initialized!");
+    }
+
+    static setLineMode(context, settings) {
+        context.lineWidth = settings.lineWidth;
+        context.globalCompositeOperation = "source-over";
+    }
+
+    static setPaddingLineMode(context, settings) {
+        context.lineWidth = settings.paddingLineWidth;
+        context.globalCompositeOperation = "destination-out";
     }
 }
 
@@ -62,6 +72,10 @@ class Empty extends Shape {
         this.connections = Empty.connections;
         this.weight = Empty.weight;
     }
+
+    draw() {
+        // Nothing to draw
+    }
 }
 
 class End extends Shape {
@@ -73,6 +87,19 @@ class End extends Shape {
 
         this.connections = End.connections;
         this.weight = End.weight;
+    }
+
+    draw(context, settings) {
+        Shape.setLineMode(context, settings);
+
+        context.beginPath();
+        context.arc(settings.shapeSize / 2, settings.shapeSize / 2, settings.shapeSize / 5, 0, 2 * Math.PI);
+        context.stroke();
+
+        context.beginPath();
+        context.moveTo(settings.shapeSize / 2, 0);
+        context.lineTo(settings.shapeSize / 2, ((settings.shapeSize / 2) - (settings.shapeSize / 4)) * this.animationProgress[0]);
+        context.stroke();
     }
 }
 
@@ -86,6 +113,18 @@ class Curve extends Shape {
         this.connections = Curve.connections;
         this.weight = Curve.weight;
     }
+
+    draw(context, settings) {
+        Shape.setLineMode(context, settings);
+
+        context.beginPath();
+        context.arc(settings.shapeSize, 0, settings.shapeSize / 2, Math.PI - (this.animationProgress[0] * 0.5 * Math.PI), Math.PI);
+        context.stroke();
+
+        context.beginPath();
+        context.arc(settings.shapeSize, 0, settings.shapeSize / 2, 0.5 * Math.PI, 0.5 * Math.PI + (this.animationProgress[1] * 0.5 * Math.PI));
+        context.stroke();
+    }
 }
 
 class Straight extends Shape {
@@ -97,6 +136,19 @@ class Straight extends Shape {
 
         this.connections = Straight.connections;
         this.weight = Straight.weight;
+    }
+
+    draw(context, settings) {
+        Shape.setLineMode(context, settings);
+
+        context.beginPath();
+        context.moveTo(settings.shapeSize / 2, 0);
+        context.lineTo(settings.shapeSize / 2, settings.shapeSize * this.animationProgress[0]);
+
+        context.moveTo(settings.shapeSize / 2, settings.shapeSize);
+        context.lineTo(settings.shapeSize / 2, settings.shapeSize - (settings.shapeSize * this.animationProgress[2]));
+
+        context.stroke();    
     }
 }
 
@@ -110,6 +162,24 @@ class Branch extends Shape {
         this.connections = Branch.connections;
         this.weight = Branch.weight;
     }
+
+    draw(context, settings) {
+        if (Math.max(...this.animationProgress) === 0) return;
+
+        Shape.setLineMode(context, settings);
+        context.beginPath();
+        context.arc(settings.shapeSize, 0, settings.shapeSize / 2, 0.5 * Math.PI, Math.PI);
+        context.stroke();
+
+        Shape.setPaddingLineMode(context, settings);
+        context.beginPath();
+        context.arc(settings.shapeSize, settings.shapeSize, settings.shapeSize / 2, Math.PI, 1.5 * Math.PI);
+        context.stroke();
+        Shape.setLineMode(context, settings);
+        context.beginPath();
+        context.arc(settings.shapeSize, settings.shapeSize, settings.shapeSize / 2, Math.PI, 1.5 * Math.PI);
+        context.stroke();  
+    }
 }
 
 class Cross extends Shape {
@@ -122,6 +192,60 @@ class Cross extends Shape {
         this.connections = Cross.connections;
         this.weight = Cross.weight;
     }
+
+    draw(context, settings) {
+        if (Math.max(...this.animationProgress) === 0) return;
+
+        // TOP RIGHT (FIRST HALF)
+        Shape.setLineMode(context, settings);
+        context.beginPath();
+        context.arc(0, 0, settings.shapeSize / 2, 0, 0.25 * Math.PI);
+        context.stroke();
+
+        // BOTTOM RIGHT
+        Shape.setPaddingLineMode(context, settings);
+        context.beginPath();
+        context.arc(settings.shapeSize, 0, settings.shapeSize / 2, 0.5 * Math.PI, Math.PI);
+        context.stroke();
+
+        Shape.setLineMode(context, settings);
+        context.beginPath();
+        context.arc(settings.shapeSize, 0, settings.shapeSize / 2, 0.5 * Math.PI, Math.PI);
+        context.stroke();
+
+        // BOTTOM LEFT
+        Shape.setPaddingLineMode(context, settings);
+        context.beginPath();
+        context.arc(settings.shapeSize, settings.shapeSize, settings.shapeSize / 2, Math.PI, 1.5 * Math.PI);
+        context.stroke();
+
+        Shape.setLineMode(context, settings);
+        context.beginPath();
+        context.arc(settings.shapeSize, settings.shapeSize, settings.shapeSize / 2, Math.PI, 1.5 * Math.PI);
+        context.stroke();
+
+        // TOP LEFT
+        Shape.setPaddingLineMode(context, settings);
+        context.beginPath();
+        context.arc(0, settings.shapeSize, settings.shapeSize / 2, 1.5 * Math.PI, 2 * Math.PI);
+        context.stroke();
+
+        Shape.setLineMode(context, settings);
+        context.beginPath();
+        context.arc(0, settings.shapeSize, settings.shapeSize / 2, 1.5 * Math.PI, 2 * Math.PI);
+        context.stroke();
+
+        // TOP RIGHT (SECOND HALF)
+        Shape.setPaddingLineMode(context, settings);
+        context.beginPath();
+        context.arc(0, 0, settings.shapeSize / 2, 0.25 * Math.PI, 0.5 * Math.PI);
+        context.stroke();
+
+        Shape.setLineMode(context, settings);
+        context.beginPath();
+        context.arc(0, 0, settings.shapeSize / 2, 0.25 * Math.PI, 0.5 * Math.PI);
+        context.stroke();     
+    }
 }
 
-export {End, Curve, Straight, Branch, Cross};
+export {Empty, End, Curve, Straight, Branch, Cross};
